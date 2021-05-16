285c702cc73839ffd132a3e186504ef71b137b64
Alfredo Patricius Tarigan <alfredoptarigan@students.usu.ac.id>
2021-05-16 16:04:20 +0700

    wip .-.

--- src/components/StorePicker.tsx
+++ src/components/StorePicker.tsx
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, BaseSyntheticEvent, useState } from 'react'
+import { RouteComponentProps } from 'react-router';
+import { getFunName } from '../helpers'
+export class StorePicker extends Component<RouteComponentProps> {
+    myInput = React.createRef<HTMLInputElement>();
 
-export class StorePicker extends Component {
+    handleSubmit = (e: BaseSyntheticEvent) => {
+        e.preventDefault();
+        const storeName = this.myInput.current?.value;
+        this.props.history.push(`/store/${storeName}`);
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.handleSubmit}>
                 <h2>Please Enter a Store</h2>
-                <input type="text" placeholder="Store Name" />
-                <button type="button">Visit Store</button>
+                <input ref={this.myInput} required type="text" placeholder="Store Name" defaultValue={getFunName()} />
+                <button type="submit">Visit Store</button>
             </form>
         )
     }
